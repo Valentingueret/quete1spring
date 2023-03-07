3fed7b721e88f23852088ce56465ae73b601994d
valentin <valentingueret06@gmail.com>
2023-03-07 10:29:49 +0100

    quest1

---
+++ quete1/src/main/java/com/example/quete1/controller/DoctorController.java
@@ -0,0 +1,30 @@
+package com.example.quete1.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String firstDoctor() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String tenDoctor() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String thirteenDoctor(){
+        return "Jodie Whittaker";
+    }
+
+
+}
